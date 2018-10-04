d5f2e71aa4e9a493cf37d4b88ee5150e48dff311
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-10-04 12:40:54 +0300

    Generate devise controllers for model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
+
   root 'home#index'
 end

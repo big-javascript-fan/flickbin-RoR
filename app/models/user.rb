3ab88e616979b6756d1da9d6c1e0634a9bfe1a5d
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-10-04 12:24:03 +0300

    Configure friendly id for model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  extend FriendlyId
+  friendly_id :channel_name, use: [:sequentially_slugged, :finders]
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates_presence_of   :channel_name
   validates_uniqueness_of :channel_name, allow_blank: true
+
+  def should_generate_new_friendly_id?
+    slug.blank? || channel_name_changed?
+  end
 end

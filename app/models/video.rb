c41d0a3ba329e962df0b0c2448c4e424a29ee2f3
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-10-08 19:25:04 +0300

    Implement cover uploader for model Video

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,8 @@ class Video < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: [:sequentially_slugged, :finders]
 
+  mount_uploader :cover, VideoCoverUploader
+
   belongs_to :user
   belongs_to :tag
 
@@ -9,7 +11,15 @@ class Video < ApplicationRecord
   validates :title, presence: true
   validates_uniqueness_of :url, scope: :tag_id
 
+  after_validation :upload_video_cover
+
   def should_generate_new_friendly_id?
     slug.blank? || title_changed?
+  end
+
+  def upload_video_cover
+    youtube_video_id = self.url.split('watch?v=').last
+    cover_url = "https://img.youtube.com/vi/#{youtube_video_id}/1.jpg"
+    self.remote_cover_url = cover_url
   end
 end

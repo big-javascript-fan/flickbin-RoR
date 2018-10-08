768594f66527a9dc706b2a8fc1127a9d8a1db3d1
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-10-08 12:41:16 +0300

    Update user registration with design

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,10 +19,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # DELETE /resource
   # def destroy
@@ -40,12 +56,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def update_resource(resource, params)
+    if params[:password].present? && params[:password] != params[:password_confirmation]
+      return resource.errors.add(:password_confirmation, :confirmation, message: "doesn't match password")
+    end
+
+    resource.update_with_password(params)
+  end
+
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:channel_name])
   end
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:channel_name, :channel_description])
+  end
+
+  def after_update_path_for(resource)
+    edit_user_registration_path
   end
 
   def after_sign_up_path_for(resource)

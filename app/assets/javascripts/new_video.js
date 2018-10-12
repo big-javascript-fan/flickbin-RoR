239bdd5891883dea4cb7c84f7ddcf94539ff1564
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-10-12 15:16:42 +0300

    Update js for post video

--- app/assets/javascripts/new_video.js
+++ app/assets/javascripts/new_video.js
@@ -1,27 +1,66 @@
 $(function() {
+  videoUrlInput = {
+    parentDiv: $('#video_url').parent(),
+    valid: false,
+    set filled(flag) {
+      if(flag == true) {
+        this.valid = true;
+        this.parentDiv.addClass('tagSelected');
+      } else {
+        this.valid = false;
+        this.parentDiv.removeClass('tagSelected');
+      }
+      submitButton.validate();
+    }
+  };
+
+  videoTagInput = {
+    parentDiv: $('#video_tag_id').parent(),
+    valid: false,
+    set filled(flag) {
+      if(flag == true) {
+        this.valid = true;
+        this.parentDiv.addClass('tagSelected');
+      } else {
+        this.valid = false;
+        this.parentDiv.removeClass('tagSelected');
+      }
+      submitButton.validate();
+    }
+  };
+
+  submitButton = {
+    element: $('#post_video'),
+    validate: function() {
+      if(videoUrlInput.valid && videoTagInput.valid) {
+        this.element.prop( "disabled", false );
+      } else {
+        this.element.prop( "disabled", true );
+      }
+    }
+  }
+
+  if($('#video_url').val().length > 0) {
+    videoUrlInput.filled = true;
+  }
+
+  if($('#video_tag_id').val().length > 0) {
+    videoTagInput.filled = true;
+  }
+
   $('#video_url').on('keyup', function() {
-    if($(this).val().length > 0 && $('#video_tag_id').val().length > 0) {
-      $(this).parent().addClass('tagSelected');
-      $('#post_video').prop( "disabled", false );
-    } else if($(this).val().length > 0) {
-      $(this).parent().addClass('tagSelected');
-      $('#post_video').prop( "disabled", true );
+    if($(this).val().length > 0) {
+      videoUrlInput.filled = true;
     } else {
-      $(this).parent().removeClass('tagSelected');
-      $('#post_video').prop( "disabled", true );
+      videoUrlInput.filled = false;
     }
   });
 
   $('#video_tag_id').on('keyup', function() {
-    if($(this).val().length > 0 && $('#video_url').val().length > 0) {
-      $(this).parent().addClass('tagSelected');
-      $('#post_video').prop( "disabled", false );
-    } else if($(this).val().length > 0) {
-      $(this).parent().addClass('tagSelected');
-      $('#post_video').prop( "disabled", true );
+    if($(this).val().length > 0) {
+      videoTagInput.filled = true;
     } else {
-      $(this).parent().removeClass('tagSelected');
-      $('#post_video').prop( "disabled", true );
+      videoTagInput.filled = false;
     }
   });
 

bb96c35ec4a977e836fbc620453984e013f42868
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-10-18 18:45:49 +0300

    Implement infinity scroll for sidebar

--- app/assets/javascripts/specific_by_templates/home.js
+++ app/assets/javascripts/specific_by_templates/home.js
@@ -1,75 +1,117 @@
 $(function() {
-  var loading = false;
-  var lastPageNumber = 1;
-  var top1TagId = $('.top_1_tag').attr('id');
-  var top2TagId = $('.top_2_tag').attr('id');
+  infiniteScrollForVideos();
+  infiniteScrollForTags();
 
-  $('.contentPanel').scroll(function(e) {
-    var scrollReachedEndOfDocument = ($('.video-feed').height() - $(this).scrollTop()) < $(window).height() - 80;
+  function infiniteScrollForVideos() {
+    var loading = false;
+    var lastPageNumber = 1;
+    var top1TagId = $('.top_1_tag').attr('id');
+    var top2TagId = $('.top_2_tag').attr('id');
 
-    if(loading) {
-      return false;
-    } else if(scrollReachedEndOfDocument) {
-      loading = true;
-      loadNextBatchOfVideos();
-    }
+    $('.contentPanel').scroll(function(e) {
+      var scrollReachedEndOfDocument = ($('.video-feed').height() - $(this).scrollTop()) < $(window).height() - 80;
 
-    function loadNextBatchOfVideos() {
-      $.get('/api/v1/home/videos', {
-        page: lastPageNumber + 1,
-        top_1_tag_id: top1TagId,
-        top_2_tag_id: top2TagId
-      }).then(function(response) {
-        var videosTop1Tag = response.videos_top_1_tag;
-        var videosTop2Tag = response.videos_top_2_tag;
-        var top1Content = '';
-        var top2Content = '';
+      if(loading) {
+        return false;
+      } else if(scrollReachedEndOfDocument) {
+        loading = true;
+        loadNextBatchOfVideos();
+      }
 
-        if(videosTop1Tag.length > 0) {
-          $.each(videosTop1Tag, function(index, video) {
-            top1Content += `
-              <li class="entityRow">
-                <div class="entityCell thumbnailCell">
-                  <a class="thumbnail" href="/videos/${video.slug}">
-                    <img alt="${video.title}" class="thumbnail" src="${video.cover_url}">
-                    <span class="playerIcon"><i class="fas fa-play"></i></span>
-                  </a>
-                </div>
-                <div class="entityCell serialText">${video.rank}</div>
-                <div class="entityCell">
-                  <a class="descText" href=href="/videos/${video.slug}">${video.title}"</a>
-                </div>
+      function loadNextBatchOfVideos() {
+        $.get('/api/v1/home/videos', {
+          page: lastPageNumber + 1,
+          top_1_tag_id: top1TagId,
+          top_2_tag_id: top2TagId
+        }).then(function(response) {
+          var videosTop1Tag = response.videos_top_1_tag;
+          var videosTop2Tag = response.videos_top_2_tag;
+          var top1Content = '';
+          var top2Content = '';
+
+          if(videosTop1Tag.length > 0) {
+            $.each(videosTop1Tag, function(index, video) {
+              top1Content += `
+                <li class="entityRow">
+                  <div class="entityCell thumbnailCell">
+                    <a class="thumbnail" href="/videos/${video.slug}">
+                      <img alt="${video.title}" class="thumbnail" src="${video.cover_url}">
+                      <span class="playerIcon"><i class="fas fa-play"></i></span>
+                    </a>
+                  </div>
+                  <div class="entityCell serialText">${video.rank}</div>
+                  <div class="entityCell">
+                    <a class="descText" href=href="/videos/${video.slug}">${video.title}"</a>
+                  </div>
+                </li>
+              `
+            });
+
+            $('.top_1_tag ul').append(top1Content)
+          }
+
+          if(videosTop2Tag.length > 0) {
+            $.each(videosTop1Tag, function(index, video) {
+              top2Content += `
+                <li class="entityRow">
+                  <div class="entityCell thumbnailCell">
+                    <a class="thumbnail" href="/videos/${video.slug}">
+                      <img alt="${video.title}" class="thumbnail" src="${video.cover_url}">
+                      <span class="playerIcon"><i class="fas fa-play"></i></span>
+                    </a>
+                  </div>
+                  <div class="entityCell serialText">${video.rank}</div>
+                  <div class="entityCell">
+                    <a class="descText" href=href="/videos/${video.slug}">${video.title}"</a>
+                  </div>
+                </li>
+              `
+            });
+
+            $('.top_2_tag ul').append(top2Content)
+          }
+
+          loading = false;
+          lastPageNumber += 1;
+        })
+      }
+    });
+  }
+
+  function infiniteScrollForTags() {
+    var loading = false;
+    var lastPageNumber = 1;
+
+    $('.leftPanel').scroll(function(e) {
+      var scrollReachedEndOfDocument = ($('.tag-feed').height() - $(this).scrollTop()) < $(window).height() - 200;
+
+      if(loading) {
+        return false;
+      } else if(scrollReachedEndOfDocument) {
+        loading = true;
+        loadNextBatchOfTags();
+      }
+    });
+
+    function loadNextBatchOfTags() {
+      $.get('/api/v1/home/tags', { page: lastPageNumber + 1}).then(function(response) {
+        var tagContent = '';
+
+        if(response.length > 0) {
+          $.each(response, function(index, tag) {
+            tagContent += `
+              <li>
+                <a href="/tags/${tag.slug}">${tag.title}</a>
               </li>
             `
           });
 
-          $('.top_1_tag ul').append(top1Content)
-        }
-
-        if(videosTop2Tag.length > 0) {
-          $.each(videosTop1Tag, function(index, video) {
-            top2Content += `
-              <li class="entityRow">
-                <div class="entityCell thumbnailCell">
-                  <a class="thumbnail" href="/videos/${video.slug}">
-                    <img alt="${video.title}" class="thumbnail" src="${video.cover_url}">
-                    <span class="playerIcon"><i class="fas fa-play"></i></span>
-                  </a>
-                </div>
-                <div class="entityCell serialText">${video.rank}</div>
-                <div class="entityCell">
-                  <a class="descText" href=href="/videos/${video.slug}">${video.title}"</a>
-                </div>
-              </li>
-            `
-          });
-
-          $('.top_2_tag ul').append(top2Content)
+          $('ul.leftPanelTags').append(tagContent)
         }
 
         loading = false;
         lastPageNumber += 1;
       })
     }
-  });
+  }
 });

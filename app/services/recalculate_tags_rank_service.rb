7097fa27cf8f5468712ee8a2a808a63f90706115
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-11-06 16:35:51 +0200

    Update rank calculating service for video

--- app/services/recalculate_tags_rank_service.rb
+++ app/services/recalculate_tags_rank_service.rb
@@ -1,11 +1,11 @@
 class RecalculateTagsRankService
   def call
     rank = 1
-    upvotes_for_last_12_hours = "votes.value = 1 AND votes.created_at BETWEEN '#{12.hours.ago.to_s}' AND '#{Time.now}'"
+    upvote_for_last_12_hours = "votes.value = 1 AND votes.created_at BETWEEN '#{12.hours.ago.to_s}' AND '#{Time.now}'"
     sorted_tags = Tag.left_outer_joins(videos: :votes)
                      .where(videos: { removed: false, untagged: false })
                      .group(:id)
-                     .order("SUM(CASE WHEN #{upvotes_for_last_12_hours} THEN 1 ELSE 0 END) + COUNT(videos) DESC")
+                     .order("SUM(CASE WHEN #{upvote_for_last_12_hours} THEN 1 ELSE 0 END) + COUNT(videos) DESC, tags.created_at DESC")
 
     sorted_tags.each do |tag|
       tag.update_attribute(:rank, rank)
--- app/services/recalculate_videos_rank_service.rb
+++ app/services/recalculate_videos_rank_service.rb
@@ -2,8 +2,15 @@ class RecalculateVideosRankService
   def call
     Tag.find_each do |tag|
       rank = 1
+      upvote_for_last_hour = "votes.value = 1 AND votes.created_at BETWEEN '#{1.hour.ago.to_s}' AND '#{Time.now}'"
+      sorted_videos_for_tag = Video.active
+                                   .tagged
+                                   .where(tag_id: tag.id)
+                                   .left_outer_joins(:votes)
+                                   .group(:id)
+                                   .order("SUM(CASE WHEN #{upvote_for_last_hour} THEN 1 ELSE 0 END) DESC, videos.created_at DESC")
 
-      Video.where(tag_id: tag.id).active.tagged.order(positive_votes_amount: :desc).each do |video|
+      sorted_videos_for_tag.each do |video|
         video.update_attribute(:rank, rank)
         rank += 1
       end

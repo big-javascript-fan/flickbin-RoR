5b609af2544e17e3824c8ccae7f48f8898343a7e
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-11-12 17:15:16 +0200

    Set sendgrid smtp for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'birdy-production.herokuapp.com', protocol: 'https' }
+  config.action_mailer.asset_host = 'https://birdy-production.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           465,
+    user_name:      ENV["SMTP_USER_NAME"],
+    password:       ENV["SMTP_PASSWORD"],
+    authentication: :plain
+  }
 end

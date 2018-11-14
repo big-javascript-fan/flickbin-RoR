08d284e4f139e8af6ca44ad6221aedb0f4727fb6
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-11-14 13:23:03 +0200

    Replace default_url_option from 'birdy-production.herokuapp.com' to 'www.flickbin.tv' for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'birdy-production.herokuapp.com', protocol: 'https' }
-  config.action_mailer.asset_host = 'https://birdy-production.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'www.flickbin.tv', protocol: 'https' }
+  config.action_mailer.asset_host = 'https://www.flickbin.tv'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:        'smtp.sendgrid.net',

ea70a1b0880d3688423116dce8030f1e8752688d
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-11-14 15:12:22 +0200

    Update production env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,10 +90,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'www.flickbin.tv', protocol: 'https' }
   config.action_mailer.asset_host = 'https://www.flickbin.tv'
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:        'smtp.sendgrid.net',
     port:           587,
+    domain:         'www.flickbin.tv',
     user_name:      ENV["SMTP_USER_NAME"],
     password:       ENV["SMTP_PASSWORD"],
     authentication: :plain

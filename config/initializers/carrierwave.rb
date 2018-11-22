cd420e2be2a4b6d2f0c25136357e2c560943abe8
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-11-22 17:18:05 +0200

    Add AWS bucket for staging

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,12 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required unless using use_iam_profile
     region:                'us-west-1'                   # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'flickbin'                     # required
+
+  if Rails.env.production?
+    config.fog_directory  = 'flickbin'
+  elsif Rails.env.staging?
+    config.fog_directory  = 'flickbinstaging'
+  end
 
   if Rails.env.staging? || Rails.env.production?
     config.storage = :fog

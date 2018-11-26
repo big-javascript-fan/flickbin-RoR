0c099ec1520a807be7db51b76ac0812af3e3955e
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-11-26 16:42:27 +0200

    Fix bug with top contributors recalculations

--- app/services/recalculate_top_contributors_service.rb
+++ app/services/recalculate_top_contributors_service.rb
@@ -1,14 +1,14 @@
 class RecalculateTopContributorsService
   def call
     Tag.find_each do |tag|
-      User.includes(accounting_videos: :tag).where(tags: { id: tag.id }).each do |user|
+      tag.users.each do |user|
         if AppConstants::NOT_RATED_USER_EMAILS.include?(user.email)
           user.contribution_points.destroy_all
           next
         end
 
         amount = 0
-        user.accounting_videos.each do |video|
+        user.accounting_videos.where(tag_id: tag.id).each do |video|
           amount += (video.positive_votes_amount + 1)
         end
 

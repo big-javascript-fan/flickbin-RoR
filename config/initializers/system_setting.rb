5471b13697b6a9aa219b16eb250b8b37d0cf4a1c
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-12-03 11:43:25 +0200

    Update init config for model SystemSetting

--- config/initializers/system_setting.rb
+++ config/initializers/system_setting.rb
@@ -1,4 +1,6 @@
-Sidekiq.set_schedule('wasp_post_job', {
-  every: "#{SystemSetting.last.data['startup_frequency_in_min_for_wasp_post']}m",
-  class: 'WaspPostJob'
-})
+if SystemSetting.table_exists? && SystemSetting.last&.data.present?
+  Sidekiq.set_schedule('wasp_post_job', {
+    every: "#{SystemSetting.last.data['startup_frequency_in_min_for_wasp_post']}m",
+    class: 'WaspPostJob'
+  })
+end

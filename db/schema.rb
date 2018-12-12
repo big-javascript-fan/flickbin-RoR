ba0e0f4c29c7bc6d1b4bdaa2c3546f41865ce886
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-12-12 13:28:17 +0200

    Add column wasp post to Video model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181206125008) do
+ActiveRecord::Schema.define(version: 20181212112651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 20181206125008) do
     t.integer "negative_votes_amount", default: 0
     t.boolean "wasp_outreach", default: false
     t.string "twitter_handle"
+    t.boolean "wasp_post", default: false
     t.index ["rank"], name: "index_videos_on_rank"
     t.index ["slug"], name: "index_videos_on_slug", unique: true
     t.index ["tag_id"], name: "index_videos_on_tag_id"

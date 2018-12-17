4af1d3b7532bd3bfca9b0f0fa645312ecb9b435b
Andrey Komarov <andrey.komarov.dev@gmail.com>
2018-12-17 20:02:38 +0200

    Add column read to notification

---
+++ db/migrate/20181217180052_add_read_column_to_notification.rb
@@ -0,0 +1,5 @@
+class AddReadColumnToNotification < ActiveRecord::Migration[5.1]
+  def change
+    add_column :notifications, :read, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181215170052) do
+ActiveRecord::Schema.define(version: 20181217180052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20181215170052) do
     t.json "event_object"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 

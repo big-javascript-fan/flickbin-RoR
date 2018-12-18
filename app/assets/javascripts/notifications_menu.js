c31f5cfa5a5fb5bf5554ed2216981e6093b1de4d
birskiy.sergey <birskiy.sergey@gmail.com>
2018-12-18 19:55:18 +0200

    add green theme dropdown notification

--- app/assets/javascripts/notifications_menu.js
+++ app/assets/javascripts/notifications_menu.js
@@ -2,13 +2,28 @@ $(function() {
   $(document).on('click', '.bell.active', function() {
     if($(this).hasClass('collapsed')) {
       $(this).removeClass('collapsed')
+      console.log($(this).parent());
       console.log('open');
       getNotifications(1);
     } else {
-      $(this).addClass('collapsed')
+      $(this).addClass('collapsed');
       console.log('closed');
     }
   });
+
+  $(document).on('click', '.bell.active', function(e){
+    e.stopPropagation();
+    $(this).parent().toggleClass('bell-dropdown-open');
+  });
+  $(document).on('click', function() {
+    $('.bell-dropdown-open').removeClass('bell-dropdown-open');
+  });
+  $(document).on('click', '.bell-dropdown-open .jq-dropdown', function(e){
+    e.stopPropagation();
+  });
+  $('.profileDropdownHolder').mouseenter( function(){
+    $('.bell-dropdown-open').removeClass('bell-dropdown-open');
+  })
 
   function getNotifications(page) {
     var ids = [];
@@ -34,3 +49,49 @@ $(function() {
     });
   }
 });
+
+
+
+
+class Dropdown {
+  constructor(selector) {
+    this.selector = selector;
+  }
+
+  init() {
+    let data = document.querySelectorAll(this.selector);
+    let self = this;
+
+    data.forEach( (item, index) => {
+      document.addEventListener('click',function (e) {
+      if ( e.target === item ) {
+        self.toggle(item)
+      } else if(e.target.parentElement == item.nextElementSibling || e.target == item.nextElementSibling){
+        e.stopPropagation();
+      } else {
+        self.hide(item)
+      }
+    })
+  })
+  }
+
+  toggle(item) {
+    console.log('asdasd')
+    item.parentElement.classList.toggle('show');
+    item.nextElementSibling.classList.toggle('show');
+  }
+  hide(item) {
+    item.parentElement.classList.remove('show');
+    item.nextElementSibling.classList.remove('show');
+  }
+}
+
+let dropdown = new Dropdown('[data-toggle]');
+
+dropdown.init();
+
+
+
+
+
+

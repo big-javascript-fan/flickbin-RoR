07f73b57284db5e697806343ad82defddfb96cd9
Andrey Komarov <andrey.komarov.dev@gmail.com>
2019-01-31 12:49:44 +0200

    Redis config fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 if Rails.env.production? || Rails.env.staging?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], reconnect_attempts: 0, network_timeout: 60  }
+    config.redis = { url: ENV['REDIS_URL'], reconnect_attempts: 1, network_timeout: 60  }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], reconnect_attempts: 0, network_timeout: 60  }
+    config.redis = { url: ENV['REDIS_URL'], reconnect_attempts: 1, network_timeout: 60  }
   end
 end
 

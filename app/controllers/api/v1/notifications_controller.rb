7b7cedb8ad5fd97d718cfddd826d8ba94fd645ca
Andrey Komarov <andrey.komarov.dev@gmail.com>
2019-02-07 16:20:35 +0200

    Small changes for notification system

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -2,17 +2,15 @@ class Api::V1::NotificationsController < Api::V1::BaseController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    notifications = current_user.notifications
-                                .limit(15)
-                                .order(created_at: :desc)
-
-    current_page_notifications = notifications.page(params[:page]).per(3)
-    render json: Api::V1::Notifications::IndexSerializer.new(notifications, current_page_notifications).call
+    top_15_notifications = current_user.top_15_notifications
+    current_page_notifications = top_15_notifications.page(params[:page]).per(3)
+    render json: Api::V1::Notifications::IndexSerializer.new(top_15_notifications, current_page_notifications).call
   end
 
   def update
     Notification.where(id: params[:ids]).update_all(read: true)
-    no_unread_notifications = current_user.notifications.unread.blank?
+    top_15_notifications = current_user.top_15_notifications
+    no_unread_notifications = top_15_notifications.count {|n| n.read == false }.zero?
     render json: { no_unread_notifications: no_unread_notifications }
   end
 end

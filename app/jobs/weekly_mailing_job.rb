5f3d661b8179a1b59b9951e19c0d2f8bdc68d92a
Andrey Komarov <andrey.komarov.dev@gmail.com>
2019-03-06 14:53:19 +0200

    Change amount of tag form 3 to5 for weekly email

--- app/jobs/weekly_mailing_job.rb
+++ app/jobs/weekly_mailing_job.rb
@@ -2,9 +2,9 @@ class WeeklyMailingJob < ApplicationJob
   queue_as :mailers
 
   def perform
-    top_5_tags = Tag.order(rank: :asc).includes(top_3_videos: :user).limit(5)
+    top_3_tags = Tag.order(rank: :asc).includes(top_3_videos: :user).limit(3)
     User.where.not(role: 'dummy').find_each do |user|
-      ApplicationMailer::weekly_mailing(user, top_5_tags).deliver_now
+      ApplicationMailer::weekly_mailing(user, top_3_tags).deliver_now
     end
   end
 end

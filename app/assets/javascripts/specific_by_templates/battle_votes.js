497f08702cc062ebf2a8213ad8239ed6b3eb0893
Denis Sobirov <denvoribos@gmail.com>
2019-03-30 01:00:10 +0200

    resolved issues of pull request

--- app/assets/javascripts/specific_by_templates/battle_votes.js
+++ app/assets/javascripts/specific_by_templates/battle_votes.js
@@ -7,23 +7,34 @@ $(function() {
         var self = this;
         var member = $(this).data('member');
         var battleId = $(".section-fight").data('battle');
-        var upvote = parseInt($(self).text());
+        var upvote = parseInt($(self).text()); 
+        var error = " You can vote once every 24 hours"
         if (member == '1') {
             $.ajax({
                 type: 'PUT',
                 url:  `/api/v1/battles/${battleId}`,
-                data: { value1: true }
-            }).then( function(response) {
-                response.status === 200 ? $(self).text(upvote + 1) : console.log(response.message);               
+                data: { value1: true },
+                error: function(xhr, message) {
+                    console.log(xhr.status);
+                    console.log(xhr.statusText + error);
+                    console.log(message);
+                }
+            }).then( function() {
+                $(self).text(upvote + 1)               
             });         
            
         } else if (member == '2') {
             $.ajax({
                 type: 'PUT',
                 url: `/api/v1/battles/${battleId}`,
-                data: { value2: true }
-            }).then( function(response) {
-                response.status === 200 ? $(self).text(upvote + 1) : console.log(response.message);
+                data: { value2: true },
+                error: function(xhr, message) {
+                    console.log(xhr.status);
+                    console.log(xhr.statusText + error);
+                    console.log(message);
+                }
+            }).then( function() {
+                $(self).text(upvote + 1)               
             });
         } else {
             alert("no");
--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -1,35 +1,21 @@
 class Api::V1::BattlesController < Api::V1::BaseController
   before_action :set_battle
-  before_action :set_voices
 
   def update
-    if vote_ip_present?(request.ip, @battle.vote_ips)
-      render json: { status: 409, message: "You can vote once every 24 hours"}.to_json
+    ip = @battle.vote_ips.find_by(ip: request.ip)
+    if (@battle.vote_ips.where(ip: request.ip).any? && Time.now <= ip.updated_at + 1.day)
+      render status: 409, json: {message: "You can vote once every 24 hours" }.to_json
     else
-      ip = VoteIp.create(battle_id: @battle.id, ip: request.ip)
+      ip.present? ? ip.update(updated_at: Time.now) : VoteIp.create(battle_id: @battle.id, ip: request.ip)
       @battle.increment!(:first_member_voices) if params[:value1] 
       @battle.increment!(:second_member_voices) if params[:value2]
-      render json: { status: 200, message: 'OK'}.to_json
+      render status: 200, json: {message: "OK" }.to_json
     end
   end
 
   private 
 
-  def vote_ip_present?(ip, vote_ips)
-    i = 0
-    while i <= vote_ips.length
-      puts i
-      return true if vote_ips[i]&.ip == ip
-      i += 1
-    end
-  end
-
   def set_battle 
     @battle = Battle.find(params[:id])
-  end
-
-  def set_voices 
-    @voices1 = @battle.first_member_voices.to_json
-    @voices2 = @battle.second_member_voices.to_json
   end
 end

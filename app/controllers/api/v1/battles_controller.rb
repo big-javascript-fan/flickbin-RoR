f73cd06a55bfa2f98fef281f4432a06e515a0dae
Denis Sobirov <denvoribos@gmail.com>
2019-03-30 15:29:37 +0200

    resolved issues fo pull request

--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -2,20 +2,19 @@ class Api::V1::BattlesController < Api::V1::BaseController
   before_action :set_battle
 
   def update
-    ip = @battle.vote_ips.find_by(ip: request.ip)
-    if (@battle.vote_ips.where(ip: request.ip).any? && Time.now <= ip.updated_at + 1.day)
+  if @battle.vote_ips.where(ip: request.ip, created_at: (Time.now - 1.day)...Time.now).any?
       render status: 409, json: {message: "You can vote once every 24 hours" }.to_json
     else
-      ip.present? ? ip.update(updated_at: Time.now) : VoteIp.create(battle_id: @battle.id, ip: request.ip)
+      VoteIp.create(battle_id: @battle.id, ip: request.ip)
       @battle.increment!(:first_member_voices) if params[:value1] 
       @battle.increment!(:second_member_voices) if params[:value2]
       render status: 200, json: {message: "OK" }.to_json
     end
   end
 
-  private 
+  private
 
-  def set_battle 
+  def set_battle
     @battle = Battle.find(params[:id])
   end
 end

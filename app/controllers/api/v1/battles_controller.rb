bd78f914c5becb3f930b3220166bc153441262f2
AntonTkachov <lookformeon@gmail.com>
2019-04-02 19:33:40 +0300

    Card #198 Restrict voting on finished battles

--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::BattlesController < Api::V1::BaseController
   before_action :set_battle
 
   def update
-    if CheckUserBattleVoteExistanceService.new(@battle, request.ip, current_user).call
+    if @battle.finished?
+      render status: 409, json: {message: "This battle was already finished" }.to_json
+    elsif CheckUserBattleVoteExistanceService.new(@battle, request.ip, current_user).call
       render status: 409, json: {message: "You can vote once every 24 hours" }.to_json
     else
       VoteIp.create(battle: @battle, ip: request.ip, user: current_user)

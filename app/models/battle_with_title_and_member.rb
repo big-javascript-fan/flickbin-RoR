0ddcdeca7aa59ba0a50bd2d6c2b17a336b41ccbe
Ihor Boiko <payk.ace@gmail.com>
2019-05-14 13:37:21 +0300

    Card #308 add battle title and optimized queries

---
+++ app/models/battle_with_title_and_member.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: battle_with_title_and_members
+#
+#  id                                          :bigint(8)        primary key
+#  final_date                                  :datetime
+#  first_members_battles_name                  :text
+#  first_members_battles_photo                 :text
+#  first_members_battles_twitter_account_name  :text
+#  first_members_battles_voices                :bigint(8)
+#  loser_members_battles_twitter_account_name  :text
+#  loser_members_battles_voices                :bigint(8)
+#  number_of_rematch_requests                  :integer
+#  second_members_battles_name                 :text
+#  second_members_battles_photo                :text
+#  second_members_battles_twitter_account_name :text
+#  second_members_battles_voices               :bigint(8)
+#  slug                                        :string
+#  status                                      :string
+#  title                                       :text
+#  winner                                      :string
+#  winner_members_battles_photo                :text
+#  winner_members_battles_twitter_account_name :text
+#  winner_members_battles_voices               :bigint(8)
+#  created_at                                  :datetime
+#  updated_at                                  :datetime
+#  first_member_id                             :integer
+#  first_members_battles_id                    :bigint(8)
+#  second_member_id                            :integer
+#  second_members_battles_id                   :bigint(8)
+#  tag_id                                      :integer
+#  winner_members_battles_id                   :bigint(8)
+#
+
+class BattleWithTitleAndMember < ApplicationRecord
+  self.primary_key = :id
+
+  belongs_to :first_member, class_name: 'BattleMember', foreign_key: :first_member_id
+  belongs_to :second_member, class_name: 'BattleMember', foreign_key: :second_member_id
+  belongs_to :tag
+  has_many :battle_votes, foreign_key: :battle_id
+  has_many :rematch_requests, foreign_key: :battle_id
+
+  mount_uploader :first_members_battles_photo, BattleMemberChannelAvatarUploader
+  mount_uploader :second_members_battles_photo, BattleMemberChannelAvatarUploader
+  mount_uploader :winner_members_battles_photo, BattleMemberChannelAvatarUploader
+
+  def readonly?
+    true
+  end
+
+  def winner_member
+    return first_member if winner == first_members_battles_name
+    return second_member if winner == second_members_battles_name
+  end
+end
---
+++ spec/factories/battle_with_titles.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: battle_with_titles
+#
+#  id                                          :bigint(8)        primary key
+#  final_date                                  :datetime
+#  first_members_battles_name                  :text
+#  first_members_battles_photo                 :text
+#  first_members_battles_twitter_account_name  :text
+#  first_members_battles_voices                :bigint(8)
+#  loser_members_battles_twitter_account_name  :text
+#  loser_members_battles_voices                :bigint(8)
+#  number_of_rematch_requests                  :integer
+#  second_members_battles_name                 :text
+#  second_members_battles_photo                :text
+#  second_members_battles_twitter_account_name :text
+#  second_members_battles_voices               :bigint(8)
+#  slug                                        :string
+#  status                                      :string
+#  title                                       :text
+#  winner                                      :string
+#  winner_members_battles_photo                :text
+#  winner_members_battles_twitter_account_name :text
+#  winner_members_battles_voices               :bigint(8)
+#  created_at                                  :datetime
+#  updated_at                                  :datetime
+#  first_member_id                             :integer
+#  first_members_battles_id                    :bigint(8)
+#  second_member_id                            :integer
+#  second_members_battles_id                   :bigint(8)
+#  tag_id                                      :integer
+#  winner_members_battles_id                   :bigint(8)
+#
+
+FactoryBot.define do
+  factory :battle_with_title do
+  end
+end

0b48f6756126022e9f3599fd206f4050d61f676c
Ihor Boiko <payk.ace@gmail.com>
2019-05-14 15:29:41 +0300

    Card #297 disable update cover in migration

--- db/migrate/20190507114304_update_cover_for_video.rb
+++ db/migrate/20190507114304_update_cover_for_video.rb
@@ -1,7 +1,7 @@
 class UpdateCoverForVideo < ActiveRecord::Migration[5.1]
   def up
-    Rake::Task['update_cover_for_video:youtube'].invoke
-    Rake::Task['update_cover_for_video:twitch'].invoke
+    # Rake::Task['update_cover_for_video:youtube'].invoke
+    # Rake::Task['update_cover_for_video:twitch'].invoke
   end
 
   def down; end

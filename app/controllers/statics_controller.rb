37c571f49fe0cd6f79f1825c385dee6fd6a71fab
Ihor Boiko <payk.ace@gmail.com>
2019-05-23 15:21:10 +0300

    load sidebar_tags in statistics page

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class StaticsController < ApplicationController
+  before_action :load_sidebar_tags
+
   def show; end
+
+  private
+
+  def load_sidebar_tags
+    @sidebar_tags ||= get_sidebar_tags(70)
+  end
 end
